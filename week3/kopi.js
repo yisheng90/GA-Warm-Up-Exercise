a1917b7a17ee9b463615fb103449e810e1dbefde
yisheng90 <yisheng@yishenglee.com>
2016-12-22 15:37:10 +0800

    update kopi.js

--- week3/kopi.js
+++ week3/kopi.js
@@ -1,8 +1,18 @@
-var order = 'tea'
+var order1 = 'tea kopi'
+var order2 = 'tea o'
+var order3 = 'tea si'
+var order4 = 'tea siewdai'
+var order5 = 'tea si ice'
+
+var order6 = 'kopi'
+var order7 = 'kopi o'
+var order8 = 'kopi si'
+var order9 = 'kopi siewdai'
+var order10 = 'kopi si ice'
 
 var recipe = {
-  kopi: 1,
-  tea:1,
+  kopi: 2,
+  tea:2,
   sugar: 2,
   evaporatedMilk: false,
   condensedMilk: true,
@@ -11,6 +21,12 @@ var recipe = {
 }
 
 function orderKopi (order) {
+
+  if (order.match(/(^kopi\s\A*\s*\btea\b)|(^tea\s\A*\s*\bkopi\b)/)) {
+    return console.log("No "+ order +" avialable.")
+  }
+
+  
   if (order.match(/\A*\s\o\s/)) {
     recipe['sugar'] = 0
   }
@@ -28,7 +44,7 @@ function orderKopi (order) {
     recipe['condensedMilk'] = false
   }
 
-  if (order.match(/\A*\s\bice\b/)) {
+  if (order.match(/(\A*\s\bice\b)|(\A*\s*\bpeng\b)/)) {
     recipe['ice'] = true
     recipe['cost'] = 1.80
   }
@@ -47,6 +63,14 @@ function orderKopi (order) {
     delete recipe['kopi']
     recipe['tea'] = 1
   }
+  
+  if (order.match(/\A*\s*\bpo\b/)) {
+    if (prder.match(/^tea/)) {
+      recipe['tea'] -= 0.5
+    } else {
+      recipe['kopi'] -= 0.5
+    }
+  }
 
   recipe['step'] = []
   for (var key in recipe) {
@@ -58,7 +82,15 @@ function orderKopi (order) {
       }
     }
   }
-  console.log(recipe)
+  console.log(order,":",recipe)
 }
 
-orderKopi(order)
+orderKopi(order1)
+orderKopi(order2)
+orderKopi(order3)
+orderKopi(order4)
+orderKopi(order5)
+orderKopi(order6)
+orderKopi(order7)
+orderKopi(order8)
+orderKopi(order9)
